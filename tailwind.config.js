7909626cf0dcc37c15562de266f1c45ca8ae8d3f
teresa <teresamarvilla@outlook.com>
2025-06-24 10:35:55 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['Garamond', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
